0e1bbae8e1b89bdb097baec317ed08eded29d253
brunogoncalvesferreira <brunogoncalvesferreira@outlook.com>
2025-09-28 19:49:13 -0300

    commit de ajustes no backend

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,13 @@ import fastify from 'fastify'
 import { createUsersRoutes } from './routes/create-users.routes'
 import { createNewEventRoutes } from './routes/create-new-event.routes'
 import { getEventsUsersRoutes } from './routes/get-events-users.routes'
+import fastifyCors from '@fastify/cors'
 
 export const app = fastify()
+
+app.register(fastifyCors, {
+  origin: ['http://localhost:5173', 'https://eventplanner.com.br'],
+})
 
 app.register(createUsersRoutes)
 app.register(createNewEventRoutes)
